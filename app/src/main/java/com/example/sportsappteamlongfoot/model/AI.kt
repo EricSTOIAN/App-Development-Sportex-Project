e212d2f966756f72ef0d2790188565d17115f9b5
Alexander B-P <98350761+Alexander006@users.noreply.github.com>
2024-11-17 22:32:06 -0500

    Implemented basic AI integration code.

--- app/src/main/java/com/example/sportsappteamlongfoot/model/AI.kt
+++ app/src/main/java/com/example/sportsappteamlongfoot/model/AI.kt
@@ -1,2 +1,23 @@
 package com.example.sportsappteamlongfoot.model
 
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.firebase.vertexai.*
+import kotlinx.coroutines.*
+
+//Refer to the following instructions: https://medium.com/@bhoomigadhiya/integrating-googles-gemini-into-the-android-app-520508975c2e
+
+val generativeModel = GenerativeModel(
+    // For text-only input, use the gemini-pro model
+    modelName = "gemini-1.5-flash",
+
+    // Access your API key as a Build Configuration variable (see "Set up your API key" above)
+    //(The BuildConfig below does not seem to have the apiKey property.
+    // We will figure out if we should directly put the API key here or try our hands
+    // again at accessing the API key from local.properties using BuildConfig.apiKey)
+    apiKey = "BuildConfig.apiKey"
+)
+
+val prompt = "Write a story about a magic backpack."
+
+//Uses coroutines
+val response = runBlocking{generativeModel.generateContent(prompt)}
