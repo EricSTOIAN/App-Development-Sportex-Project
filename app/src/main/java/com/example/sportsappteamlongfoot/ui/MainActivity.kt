979a9af28cf619f30e0130a4e3a58241c703dd75
Alexander B-P <98350761+Alexander006@users.noreply.github.com>
2024-12-01 22:48:59 -0500

    Updated imports.

--- app/src/main/java/com/example/sportsappteamlongfoot/ui/MainActivity.kt
+++ app/src/main/java/com/example/sportsappteamlongfoot/ui/MainActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
