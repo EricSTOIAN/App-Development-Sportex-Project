be06d57b97dbfaa99b25cc975c0268a3aa61c37f
Alexander B-P <98350761+Alexander006@users.noreply.github.com>
2024-12-07 11:43:29 -0500

    Implemented setup code for Goal and Workout details screen.

--- app/src/main/java/com/example/sportsappteamlongfoot/ui/screens/GoalDetailsScreen.kt
+++ app/src/main/java/com/example/sportsappteamlongfoot/ui/screens/GoalDetailsScreen.kt
@@ -1,32 +1,96 @@
 package com.example.sportsappteamlongfoot.ui.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sportsappteamlongfoot.data.Goal
+
+
+
+//For debugging purposes
+private val goals: List<Goal> = listOf(
+    Goal("Run 25 km","Running","December 19 2029","Preparing for a competition"),
+    Goal("Bike 50km ","Biking","March 2 2021","Just for fun"),
+    Goal("Do 60 Push-Ups","Exercises","June 7 2027","To be stronger")
+)
 
 @Composable
 fun GoalDetailsScreen(modifier:Modifier){
-    Box(modifier.background(Color.White)){
-        Column(){
-            Text(text="Goals")
+    val scrollStateBox = rememberScrollState()
+    val scrollStateLazyColumnsActiveGoals = rememberScrollState()
+    val scrollStateLazyColumnsUpcomingGoals = rememberScrollState()
+
+
+    Box(modifier.background(Color.White)
+       ){
+        Column(modifier = Modifier.padding(20.dp)){
+            Text(text="Goals",
+                fontSize = 45.sp)
 
             Row(modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)){
-                Text(text= "Active Goals",Modifier.padding(top = 10.dp, bottom = 10.dp))
+                Text(text= "Active Goals",
+                    fontSize = 30.sp,
+                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp))
             }
 
-
+            LazyColumn (modifier = Modifier
+                .padding(top = 15.dp, bottom = 15.dp)) {
+                items(goals){ goal ->
+                    Column(modifier
+                        .padding(top = 15.dp, bottom = 15.dp)
+                        .border(
+                            border = BorderStroke(8.dp, Color.LightGray),
+                            shape = RoundedCornerShape(16.dp)
+                        )){
+                        Column(modifier
+                            .padding(start = 20.dp,top = 25.dp, end= 15.dp,bottom= 20.dp)){
+                            Text(text = goal.name)
+                            Text(text = goal.description, modifier.padding(top = 10.dp))
+                        }
+                    }
+                }
+            }
             Row(modifier = Modifier.padding(top = 10.dp, bottom = 10.dp)){
-                Text(text= "Upcoming Goals", Modifier.padding(top = 10.dp, bottom = 10.dp))
+                Text(text= "Upcoming Goals",
+                    fontSize = 30.sp,
+                    modifier = Modifier.padding(top = 10.dp, bottom = 10.dp))
+            }
+
+            LazyColumn (modifier = Modifier
+                .padding(top = 15.dp, bottom = 15.dp)
+            ) {
+
+                items(goals){ goal ->
+                    Column(modifier
+                        .padding(top = 15.dp, bottom = 15.dp)
+                        .border(
+                            border = BorderStroke(8.dp, Color.LightGray),
+                            shape = RoundedCornerShape(16.dp)
+                        )){
+                        Column(modifier
+                            .padding(start = 20.dp,top = 25.dp, end= 15.dp,bottom= 20.dp)){
+                            Text(text = goal.name)
+                            Text(text = goal.description, modifier.padding(top = 10.dp))
+                        }
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/sportsappteamlongfoot/ui/screens/WorkoutDetailsScreen.kt
+++ app/src/main/java/com/example/sportsappteamlongfoot/ui/screens/WorkoutDetailsScreen.kt
@@ -1,2 +1,70 @@
 package com.example.sportsappteamlongfoot.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.sportsappteamlongfoot.data.Workout
+
+
+//For debugging purposes
+private val workouts: List<Workout> = listOf(
+    Workout("Run 5 km","Running","July 4 2024","First day of running"),
+    Workout("Bike 10 km ","Biking","July 15 2024","First day of biking. I want to gradually become better"),
+    Workout("Do 20 Push-Ups","Exercises","August 6 2024","I will do 20 pushups for today.")
+)
+
+@Composable
+fun WorkoutDetailsScreen(modifier:Modifier){
+    val scrollStateBox = rememberScrollState()
+    val scrollStateLazyColumnsActiveGoals = rememberScrollState()
+    val scrollStateLazyColumnsUpcomingGoals = rememberScrollState()
+
+
+    Box(modifier.background(Color.White)
+    ){
+        Column(modifier = Modifier.padding(20.dp)){
+            Text(text="Workouts",
+                fontSize = 45.sp)
+
+
+            LazyColumn (modifier = Modifier
+                .padding(top = 15.dp, bottom = 15.dp)) {
+                items(workouts){ workout ->
+                    Column(modifier
+                        .padding(top = 15.dp, bottom = 15.dp)
+                        .border(
+                            border = BorderStroke(8.dp, Color.LightGray),
+                            shape = RoundedCornerShape(16.dp)
+                        )){
+                        Column(modifier
+                            .padding(start = 20.dp,top = 25.dp, end= 15.dp,bottom= 20.dp)){
+                            Text(text = workout.name)
+                            Text(text = workout.description, modifier.padding(top = 10.dp))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WorkoutDetailsPreview(){
+    WorkoutDetailsScreen(Modifier.fillMaxSize())
+}
