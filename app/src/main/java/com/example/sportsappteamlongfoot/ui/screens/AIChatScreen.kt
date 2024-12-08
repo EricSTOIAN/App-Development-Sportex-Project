699e4c48fe82de2e69a55e841d2f802282f8ee23
Alexander B-P <98350761+Alexander006@users.noreply.github.com>
2024-12-07 20:47:37 -0500

    Moved AIChatScreen to screen directory.

--- app/src/main/java/com/example/sportsappteamlongfoot/ui/screens/AIChatScreen.kt
+++ app/src/main/java/com/example/sportsappteamlongfoot/ui/screens/AIChatScreen.kt
@@ -1,26 +1,20 @@
-package com.example.sportsappteamlongfoot.ui
+package com.example.sportsappteamlongfoot.ui.screens
 
-import android.content.res.Resources
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -32,10 +26,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,8 +38,6 @@ import com.example.sportsappteamlongfoot.model.AIModel
 import com.example.sportsappteamlongfoot.model.Workout
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import com.example.sportsappteamlongfoot.ui.*
-
 
 
 private var workoutDatastoreInstance: Workout = Workout("","","","");
